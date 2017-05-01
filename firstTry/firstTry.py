d9b37996158ff98d4fba2fb6984513560ac6158a
poming\pomingpo <roy989898@gmail.com>
2017-05-01 23:57:41 +0800

    create stores list

--- firstTry/firstTry.py
+++ firstTry/firstTry.py
@@ -1,6 +1,18 @@
 from flask import Flask
 
 app = Flask(__name__)
+stores = [
+    {
+        'name': 'My Wonderful Store',
+        'items': [
+            {
+                'name': 'My Item',
+                'price': 15.99
+            }
+        ]
+
+    }
+]
 
 
 # @app.route('/')
