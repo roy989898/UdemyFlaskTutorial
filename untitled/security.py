ae7e156e6f7d6ac5f28465959de02b6400ae7046
poming\pomingpo <roy989898@gmail.com>
2017-05-02 20:33:35 +0800

    make the user inf save in database

--- untitled/security.py
+++ untitled/security.py
@@ -1,33 +1,13 @@
 from user import User
 from werkzeug.security import safe_str_cmp
 
-users = [
-    User(2, 'bob', 'asdf')
-]
-
-username_mapping = {u.username: u for u in users}
-
-# 'bob': {
-#         'id': 1,
-#         'username': 'bob',
-#         'password': 'asdf'
-#     }
-userid_mapping = {u.id: u for u in users}
-
-
-# 1: {
-#     'id': 1,
-#     'username': 'bob',
-#     'password': 'asdf'
-# }
-
 
 def authenticate(username, password):
-    user = username_mapping.get(username, None)
+    user = User.find_by_username(username)
     if user is not None and safe_str_cmp(password, user.password):
         return user
 
 
 def identity(payload):
     user_id = payload['identity']
-    return userid_mapping.get(user_id, None)
+    return User.find_by_id(user_id)
--- untitled/user.py
+++ untitled/user.py
@@ -1,5 +1,38 @@
+import sqlite3
+
+
 class User:
-    def __init__(self, id, username, password):
-        self.id = id
+    def __init__(self, _id, username, password):
+        self.id = _id
         self.username = username
         self.password = password
+
+    @classmethod
+    def find_by_username(cls, username):
+        connection = sqlite3.connect('data.sqlite')
+        cursor = connection.cursor()
+        query = "SELECT * FROM users WHERE user=?"
+        result = cursor.execute(query, (username,))  # , ensure th is a tupe
+        row = result.fetchone()  # only get the first one
+        if row is not None:
+            user = cls(row[0], row[1], row[2])
+        else:
+            user = None
+
+        connection.close()
+        return user
+
+    @classmethod
+    def find_by_id(cls, id):
+        connection = sqlite3.connect('data.sqlite')
+        cursor = connection.cursor()
+        query = "SELECT * FROM users WHERE id=?"
+        result = cursor.execute(query, (id,))  # , ensure th is a tupe
+        row = result.fetchone()  # only get the first one
+        if row is not None:
+            user = cls(row[0], row[1], row[2])
+        else:
+            user = None
+
+        connection.close()
+        return user
