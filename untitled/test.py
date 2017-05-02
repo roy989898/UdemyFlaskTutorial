a1326f6a6e09c9a571d6c4775be09aef76687297
poming\pomingpo <roy989898@gmail.com>
2017-05-02 20:02:00 +0800

    add comment

--- untitled/test.py
+++ untitled/test.py
@@ -1,24 +1,30 @@
 import sqlite3
 
+# create the db and open the connection
 connection = sqlite3.connect('data.sqlite')
 cursor = connection.cursor()
-create_table = "CREATE TABLE IF NOT EXISTS users(id INT,user TEXT,password TEXT)"
 
+# create table
+create_table = "CREATE TABLE IF NOT EXISTS users(id INT,user TEXT,password TEXT)"
 cursor.execute(create_table)
 
+# insert a row
 user = (1, 'jose', 'asdf')
 insert_query = "INSERT INTO users VALUES (?,?,?)"
 cursor.execute(insert_query, user)
 
+# insert many row
 users = {
     (2, 'rolf', 'asdf'),
     (3, 'anne', 'xyz')
 }
 cursor.executemany(insert_query, users)
 
+# query
 select_query = "SELECT * FROM users"
 for row in cursor.execute(select_query):
     print(row)
 
+# close the connection
 connection.commit()
 connection.close()
